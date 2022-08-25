037d8cb9b52700a41840e5d7351bb1abb489af0b
joyceams <joyceaketzalli@gmail.com>
2022-08-25 16:05:13 -0500

    a ver si con esto ya sale toda la info bien

--- src/main.js
+++ src/main.js
@@ -3,32 +3,75 @@ import allData from './data/lol/lol.js'
 const main = document.querySelector('main')
 //const root = document.getElementById('root')
 
-const data=allData.data 
+let campeones = []// Haciendo array
 
+const data=allData.data //Se importa la data en una variable "data"
 
 const keys = Object.keys(data);
+//console.log(keys.at(0)) // -> Sirve para acceder solamente al elemento seleccionado
+
+
+for (let i = 0; i < keys.length; i++)
+{
+    campeones.push(data[keys[i]]) //Haciendo push a champions
+
+}
+
+console.log(campeones)
+campeones.forEach (campeon =>{
+    console.log(campeon.name);
+})
+
+const personajes = Object.entries(data);
+console.log(personajes)
+
+//let roles = [];
 
 for (let i = 0; i < keys.length; i++){
     let key = keys[i];
     let characters = data[key].name;
     let images = data[key].splash;
     let roles = data[key].tags
-console.log(roles)
-champions (characters, images, roles)
+    //roles.push(data[key].tags)
+    console.log(roles);
+
+    champions (characters, images, roles)
 }
 
-function champions(characters, images, roles){
-    
-    const card = document.createElement('div')
-    card.className = "card"
 
-    const img = document.createElement('img')
+//console.log(roles[0][0]);
+
+function champions(characters, images, roles){
+
+    const card = document.createElement('div')
+	function champions(characters, images, roles){
     img.src = images;
 
     const titleName = document.createElement('h3')
-    titleName.textContent = characters; //+ ' ' + roles;
+    titleName.textContent = characters + ' ' + roles;
 
     card.append(img, titleName)
     main.append(card)
 }
+}
 
+
+
+////////// Haciendo filtrado
+let filtrados = [] // Solo los campeones que coincidan con el rol seleccionado
+const roles = ["Mage","Support","Tank","Fighter","Marksman","Assassin"]  //Definiendo un array con todos los roles posibles
+for (let i = 0; i < keys.length; i++){
+    let key = keys[i];
+    for (let j=0; j < data[key].tags.length; j++) {
+        if( data[key].tags[j] == roles[3]) {
+            filtrados.push(data[key].name) // Obteniendo el nombre de los campeones que coinciden con el rol seleccionado
+            break
+        }
+    }
+}
+
+console.log(filtrados);
+
+// function OnSelected() {
+//if( data[key].tags[j] == document.getElementByID("CuadroDeseleccion").string())
+//}
