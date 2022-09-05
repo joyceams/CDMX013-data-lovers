12995cf285a0fb0ca5f5ad1e14fe08c20d771ae3
ElizabethOchoa2000 <kidsaochoa@gmail.com>
2022-09-05 16:12:12 -0500

    Agregra una nueva funcion llamada filtradoConjunto

--- src/data.js
+++ src/data.js
@@ -1,23 +1,50 @@
 // estas funciones son de ejemplo
 
-export const filterByRol = (rolSeleccionado, campeones) => {
-  let todosLosRoles=[]
-    if(rolSeleccionado == "All"){
+export const filterByRol = (campeones, rolSeleccionado, difficultySeleccionado) => {
+    console.log("================");
+    console.log(rolSeleccionado);
+    console.log(difficultySeleccionado);
+    console.log("================");
+    let todosLosRoles=[]
+    if(rolSeleccionado == "All" && difficultySeleccionado=="All"){
         todosLosRoles = campeones 
     }
     else{
         todosLosRoles = campeones.filter(campeon =>{
         let resultado = false
+        if(rolSeleccionado=="All") { return campeon.info.difficulty == Number(difficultySeleccionado)}
+        else if(difficultySeleccionado=="All")
+        {
             for (let j=0; j <campeon.tags.length; j++) {
-                if( campeon.tags[j] == rolSeleccionado) {
+                if(campeon.tags[j] == rolSeleccionado) {
                     resultado = true
                     break
                 }
             } 
-            return resultado
+        }
+        else {
+            for (let j=0; j <campeon.tags.length; j++) {
+                if( (campeon.tags[j] == rolSeleccionado) && (campeon.info.difficulty == Number(difficultySeleccionado))) {
+                    resultado = true
+                    break
+                }
+            }
+        }
+        return resultado
     })
-  }
-  return todosLosRoles
+    }
+    return todosLosRoles
 }
 
-//export const filterByDifficulty
+// export const filterByDifficulty
+// export const filterDificultad=(difficultySeleccionado, campeones)=>{
+//     return campeones.filter(campeon =>{       
+//     //return campeon.info.difficulty == Number (difficultySeleccionado)
+//     if(difficultySeleccionado == "All"){
+//         return true
+//     }
+//     else {
+//         return campeon.info.difficulty == Number(difficultySeleccionado)
+//     }
+// })
+// }
