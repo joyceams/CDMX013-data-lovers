e2dc402aa5fa280c32b348c8be462523540155d2
joyceams <joyceaketzalli@gmail.com>
2022-09-07 14:06:42 -0500

    Actualiza el css y html

--- src/data.js
+++ src/data.js
@@ -3,52 +3,58 @@ export const filterByRol = (campeones, rolSeleccionado, difficultySeleccionado) 
     console.log(rolSeleccionado);
     console.log(difficultySeleccionado);
     console.log("================");
-    let todosLosRoles=[]
-    if(rolSeleccionado == "All" && difficultySeleccionado=="All"){
-        todosLosRoles = campeones 
+    let todosLosRoles = []
+    if (rolSeleccionado == "All" && difficultySeleccionado == "All") {
+        todosLosRoles = campeones
     }
-    else{
-        todosLosRoles = campeones.filter(campeon =>{
-        let resultado = false
-        if(rolSeleccionado=="All") {
-          return campeon.info.difficulty == Number(difficultySeleccionado)}
-        else if(difficultySeleccionado=="All")
-        {
-            for (let j=0; j <campeon.tags.length; j++) {
-                if(campeon.tags[j] == rolSeleccionado) {
-                    resultado = true
-                    break
-                }
-            } 
-        }
-        else {
-            for (let j=0; j <campeon.tags.length; j++) {
-                if( (campeon.tags[j] == rolSeleccionado) && (campeon.info.difficulty == Number(difficultySeleccionado))) {
-                    resultado = true
-                    break
+    else {
+        todosLosRoles = campeones.filter(campeon => {
+            let resultado = false
+            if (rolSeleccionado == "All") {
+                return campeon.info.difficulty == Number(difficultySeleccionado)
+            }
+            else if (difficultySeleccionado == "All") {
+                for (let j = 0; j < campeon.tags.length; j++) {
+                    if (campeon.tags[j] == rolSeleccionado) {
+                        resultado = true
+                        break
+                    }
                 }
             }
-        }
-        return resultado
-    })
+            else {
+                for (let j = 0; j < campeon.tags.length; j++) {
+                    if ((campeon.tags[j] == rolSeleccionado) && (campeon.info.difficulty == Number(difficultySeleccionado))) {
+                        resultado = true
+                        break
+                    }
+                }
+            }
+            return resultado
+        })
     }
     return todosLosRoles
 }
 
 export const sortChampionsAscend = (campeones) => {
-  campeones.sort ((a, b) => {
-    if (a.name < b.name) {
-        return -1;
-    }else 
-        return 1;
+    campeones.sort((a, b) => {
+        if (a.name < b.name) {
+            return -1;
+        } else
+            return 1;
     }
-)}
+    )
+    return campeones.sort
+}
 
-export const sortChampionsDescend= (campeones) => {
-  campeones.sort ((a, b) => {
-    if (a.name > b.name) {
-         return -1;
-    }else 
-        return 1;
+export const sortChampionsDescend = (campeones) => {
+    campeones.sort((a, b) => {
+        if (a.name > b.name) {
+            return -1;
+        } else
+            return 1;
     }
-)}
+    )
+    return campeones.sort
+}
+
+export {filterByRol, sortChampionsAscend, sortChampionsDescend}
--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import allData from './data/lol/lol.js'
 import { filterByRol, sortChampionsAscend, sortChampionsDescend } from './data.js';
 
 const main = document.querySelector('main')
-const data=allData.data //Se importa la data en una variable "data"
+const data = allData.data //Se importa la data en una variable "data"
 
 let campeones = [];// Haciendo array
 
-let roleSelector = document.getElementById ("roles")
+let roleSelector = document.getElementById("roles")
 let difficultySelector = document.getElementById("difficulty")
 let sortSelector = document.getElementById("sorting")
 
@@ -18,7 +18,7 @@ const keys = Object.keys(data);
 //console.log(keys.at(0)) // -> Sirve para acceder solamente al elemento seleccionado
 
 //------- Lo que hicimos con Chris
-for (let i = 0; i < keys.length; i++){
+for (let i = 0; i < keys.length; i++) {
     campeones.push(data[keys[i]]) //Haciendo push a champions
 }
 
@@ -26,14 +26,14 @@ console.log('campeones', campeones)
 
 function filtradoConjunto() {
 
-    const campeonesFiltrados= filterByRol(campeones, rolSeleccionado, difficultySeleccionado);
+    const campeonesFiltrados = filterByRol(campeones, rolSeleccionado, difficultySeleccionado);
 
-    main.innerHTML='';
-    campeonesFiltrados.forEach(campeon=>{
+    main.innerHTML = '';
+    campeonesFiltrados.forEach(campeon => {
         let characters = campeon.name;
         let images = campeon.splash;
         let roles = campeon.tags;
-        champions (characters, images, roles)
+        champions(characters, images, roles)
     })
 }
 
@@ -42,7 +42,7 @@ roleSelector.addEventListener('change', (event) => {
     filtradoConjunto();
 });
 
-difficultySelector.addEventListener('change', (event) =>{
+difficultySelector.addEventListener('change', (event) => {
     difficultySeleccionado = event.target.value // Para cuando cambia la difficultad
     filtradoConjunto();
 });
@@ -51,21 +51,21 @@ difficultySelector.addEventListener('change', (event) =>{
 
 // Para el ordenado
 
-sortSelector.addEventListener('change', (event) =>{
+sortSelector.addEventListener('change', (event) => {
     const campeonesCopy = [...campeones]
     if (event.target.value === 'A to Z') {
         sortChampionsAscend(campeones);
-    /*campeones.sort ((a, b) => {
-        if (a.name < b.name) {
-            return -1;
-        }else 
-            return 1;
-        }
-    )*/
-}
+        /*campeones.sort ((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+            }else 
+                return 1;
+            }
+        )*/
+    }
 
 
-    if (event.target.value === 'Z to A'){
+    if (event.target.value === 'Z to A') {
         sortChampionsDescend(campeones);
     /*campeones.sort ((a, b) => {
         if (a.name > b.name) {
@@ -75,24 +75,24 @@ sortSelector.addEventListener('change', (event) =>{
         }
     )*/}
     filtradoConjunto(campeonesCopy)
-    });
+});
 
 
 //---------- ciclo for y función para jalar la data a las tarjetas
-for (let i = 0; i < keys.length; i++){
+for (let i = 0; i < keys.length; i++) {
     let key = keys[i];
     let characters = data[key].name;
     let images = data[key].splash;
     let roles = data[key].tags;
 
-    champions (characters, images, roles)
+    champions(characters, images, roles)
 }
 
-function champions(characters, images, roles){
+function champions(characters, images, roles) {
 
     const card = document.createElement('div')
     card.className = "card";
-    
+
     const img = document.createElement('img')
     img.src = images;
 
@@ -106,4 +106,3 @@ function champions(characters, images, roles){
     card.append(img, titleName)
     main.append(card)
 }
- 
