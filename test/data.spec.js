dcb1e321420705c15890233ff9220a3787f6f30c
ElizabethOchoa2000 <kidsaochoa@gmail.com>
2022-09-12 21:19:05 -0500

    Esta rama se usa para hacer guthub pages nuestro sitio

--- test/data.spec.js
+++ test/data.spec.js
@@ -68,7 +68,7 @@ describe('anotherExample', () => {
   it('is a function', () => {
     expect(typeof anotherExample).toBe('function')
   })
- 
+  
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG')
   })
